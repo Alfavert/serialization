f54f7caa41802a807b7ff22f5da1df159744abe8
Elbek Ganiev <twobealfa@yandex.ru>
2024-04-08 11:29:23 +0300

    added separeted file for Serialization S57CellModule

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ import S57Library.objects.*;
 import S57Library.records.S57DataRecordEntryMap;
 import S57Library.records.S57LogicalRecord;
 import S57Library.basics.E_S57IntededUsage;
+import S57Library.serialize.S57CellModuleSerialization;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -47,7 +49,7 @@ public class Main {
             PrintSpatial(feature, spatialList);
         }
         try {
-            serialization();
+            S57CellModuleSerialization.serialization();
         }catch (Exception e){
             throw new RuntimeException(e);
     }
@@ -104,94 +106,5 @@ public class Main {
             else
                 System.out.println("Not found");
         }
-    }
-
-    public static void serialization() throws IOException {
-        try {
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("S57CellModule.txt"));
-        S57CellModule cellModule = new S57CellModule();
-//        cellModule.setIntendedUsage("dfgsdfg");
-//        String tag = "dfgsdfg";
-//        byte[] fieldData = new byte [1];
-//        S57ByteBuffer buffer = new S57ByteBuffer(1);
-//
-//        S57LogicalRecord logicalRecord = new S57LogicalRecord() {
-//            @Override
-//            protected boolean checkValidity() {
-//                return false;
-//            }
-//        };
-//        S57DDRDataDescriptiveField fieldDefinition = new S57DDRDataDescriptiveField(1, buffer, logicalRecord);
-//        S57FieldDSID s57FieldDSID = new S57FieldDSID();
-//
-//        cellModule.setDSID();
-//
-//
-//        cellModule
-        int lg = 100;
-        S57ByteBuffer buffer = new S57ByteBuffer(lg);
-//        byte[] array = new byte[20];
-//        new Random().nextBytes(array);
-        byte[] array = new byte[20];
-        Random random = new Random();
-        ByteBuffer buffer1 = ByteBuffer.wrap(array);
-        for (int i = 0; i < array.length; i += 4) {
-            int randomIntValue = random.nextInt(); // генерируем случайное int значение
-            buffer1.putInt(randomIntValue);
-        }
-
-        S57LogicalRecord record = new S57LogicalRecord(array) {
-            @Override
-            protected boolean checkValidity() {
-                return false;
-            }
-        };
-        int pos = 20;
-        S57DataRecordEntryMap dataRecordEntryMap = new S57DataRecordEntryMap(array, pos);
-        int index = 1;
-        S57DDRDataDescriptiveField fieldDefinition = new S57DDRDataDescriptiveField(index, buffer, record);
-        String tag = "testTag";
-        byte[] fieldData = new byte[50];
-        new Random().nextBytes(fieldData);
-
-
-
-            S57FieldDSID fieldDSID = new S57FieldDSID(tag, fieldData, fieldDefinition);
-            fieldDSID.intendedUsage.name();
-            fieldDSID.datasetName = "123";
-            fieldDSID.editionNumber = "wad";
-            fieldDSID.updateNumber = "fre";
-            fieldDSID.updateApplicationDate = "fre";
-            fieldDSID.issueDate = "fre";
-            fieldDSID.decode();
-            cellModule.setDSID(fieldDSID);
-
-            S57FieldDSPM dspm = new S57FieldDSPM(tag, fieldData, fieldDefinition);
-            dspm.verticalDatum = 3;
-            dspm.coordinateMultiplicationFactor = 4;
-            dspm.soundingMultiplicationFactor = 5;
-            dspm.compilationScaleOfData = 2;
-
-            S57FieldDSSI fieldDSSI = new S57FieldDSSI(tag, fieldData, fieldDefinition);
-            fieldDSSI.dataStructure = 123;
-            fieldDSSI.attfLexicalLevel = 321;
-            fieldDSSI.nationalLexicalLevel = 222;
-            fieldDSSI.numberOfMetaRecords = 111;
-            fieldDSSI.numberOfCartographicRecords = 333;
-            fieldDSSI.numberOfGeoRecords = 213;
-            fieldDSSI.numberOfCollectionrecords = 321;
-            objectOutputStream.writeObject(cellModule);
-            objectOutputStream.writeObject(buffer);
-            objectOutputStream.writeObject(dataRecordEntryMap);
-            objectOutputStream.writeObject(fieldDefinition);
-            objectOutputStream.writeObject(fieldDSID);
-            objectOutputStream.writeObject(dspm);
-            objectOutputStream.writeObject(fieldDSSI);
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-
-
-
     }
 }// end Main
--- src/S57Library/serialize/S57CellModuleSerialization.java
+++ src/S57Library/serialize/S57CellModuleSerialization.java
@@ -1,4 +1,81 @@
 package S57Library.serialize;
 
-public class S57CellModuleSerialization {
+import S57Library.fiedsRecords.S57FieldDSID;
+import S57Library.fiedsRecords.S57FieldDSPM;
+import S57Library.fiedsRecords.S57FieldDSSI;
+import S57Library.fiedsRecords.descriptive.S57DDRDataDescriptiveField;
+import S57Library.files.S57ByteBuffer;
+import S57Library.files.S57CellModule;
+import S57Library.records.S57DataRecordEntryMap;
+import S57Library.records.S57LogicalRecord;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
+public class S57CellModuleSerialization implements Serializable {
+    public static void serialization() throws IOException {
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("S57CellModule.txt"));
+            S57CellModule cellModule = new S57CellModule();
+            int lg = 100;
+            S57ByteBuffer buffer = new S57ByteBuffer(lg);
+            byte[] array = new byte[20];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (byte) (i * 10);
+            }
+
+            S57LogicalRecord record = new S57LogicalRecord(array) {
+                @Override
+                protected boolean checkValidity() {
+                    return false;
+                }
+            };
+            int pos = 20;
+            S57DataRecordEntryMap dataRecordEntryMap = new S57DataRecordEntryMap(array, pos);
+            int index = 1;
+            S57DDRDataDescriptiveField fieldDefinition = new S57DDRDataDescriptiveField(index, buffer, record);
+            String tag = "testTag";
+            byte[] fieldData = new byte[50];
+            new Random().nextBytes(fieldData);
+
+
+            S57FieldDSID fieldDSID = new S57FieldDSID(tag, fieldData, fieldDefinition);
+            fieldDSID.intendedUsage.name();
+            fieldDSID.datasetName = "123";
+            fieldDSID.editionNumber = "wad";
+            fieldDSID.updateNumber = "fre";
+            fieldDSID.updateApplicationDate = "fre";
+            fieldDSID.issueDate = "fre";
+            fieldDSID.decode();
+            cellModule.setDSID(fieldDSID);
+
+            S57FieldDSPM dspm = new S57FieldDSPM(tag, fieldData, fieldDefinition);
+            dspm.verticalDatum = 3;
+            dspm.coordinateMultiplicationFactor = 4;
+            dspm.soundingMultiplicationFactor = 5;
+            dspm.compilationScaleOfData = 2;
+
+            S57FieldDSSI fieldDSSI = new S57FieldDSSI(tag, fieldData, fieldDefinition);
+            fieldDSSI.dataStructure = 123;
+            fieldDSSI.attfLexicalLevel = 321;
+            fieldDSSI.nationalLexicalLevel = 222;
+            fieldDSSI.numberOfMetaRecords = 111;
+            fieldDSSI.numberOfCartographicRecords = 333;
+            fieldDSSI.numberOfGeoRecords = 213;
+            fieldDSSI.numberOfCollectionrecords = 321;
+            objectOutputStream.writeObject(cellModule);
+            objectOutputStream.writeObject(buffer);
+            objectOutputStream.writeObject(dataRecordEntryMap);
+            objectOutputStream.writeObject(fieldDefinition);
+            objectOutputStream.writeObject(fieldDSID);
+            objectOutputStream.writeObject(dspm);
+            objectOutputStream.writeObject(fieldDSSI);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
